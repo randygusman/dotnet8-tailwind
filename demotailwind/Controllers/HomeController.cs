2e90d017452f31b50c3deee3074398aea648bb68
Randy Gusman <randygusman04@gmail.com>
2025-04-03 10:13:59 +0700

    Initial commit with MVC .NET 8 and Tailwind

---
+++ demotailwind/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using demotailwind.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace demotailwind.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
